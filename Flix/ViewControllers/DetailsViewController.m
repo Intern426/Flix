1fce94d374a23c04c1ab4b83dfb1532fc4f7eeff
Kalkidan Tamirat <kalkidant@fb.com>
2021-06-23 14:36:53 -0700

    Added another view controller that has the poster/backdrop set up

---
+++ Flix/ViewControllers/DetailsViewController.m
@@ -0,0 +1,56 @@
+//
+//  DetailsViewController.m
+//  Flix
+//
+//  Created by Kalkidan Tamirat on 6/23/21.
+//
+
+#import "DetailsViewController.h"
+#import "UIImageView+AFNetworking.h"
+
+@interface DetailsViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *backdropView;
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+    
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    
+    NSString *backdropUrlString = self.movie[@"backdrop_path"];
+    NSString *fullBackdropURLString = [baseURLString stringByAppendingString:backdropUrlString];
+    NSURL *backdropURL = [NSURL URLWithString:fullBackdropURLString]; // checks to make sure it's a valid URL
+    
+    
+    
+    NSString *posterUrlString = self.movie[@"poster_path"];
+    NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterUrlString];
+    NSURL *posterURL = [NSURL URLWithString:fullPosterURLString]; // checks to make sure it's a valid URL
+    
+    [self.backdropView setImageWithURL:backdropURL];
+    [self.posterView setImageWithURL:posterURL];
+    
+    self.titleLabel.text = self.movie[@"title"];
+    self.synopsisLabel.text = self.movie[@"overview"];
+
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- Flix/ViewControllers/MoviesViewController.m
+++ Flix/ViewControllers/MoviesViewController.m
@@ -7,6 +7,7 @@
 
 #import "MoviesViewController.h"
 #import "MovieCell.h"
+#import "DetailsViewController.h"
 #import "UIImageView+AFNetworking.h"
 
 @interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
@@ -90,14 +91,26 @@
     return cell;
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender { // Method Function: About to leave, any info you wanna send?
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    UITableViewCell *tappedCell = sender;
+    
+    NSIndexPath *indexPath = [self.tableView indexPathForCell:tappedCell];
+    NSDictionary *movie = self.movies[indexPath.row];
+    
+    DetailsViewController *detailsViewController = [segue destinationViewController];
+    
+    detailsViewController.movie = movie;
+    
+    
+    NSLog(@"Tapping on a movie!");
+    
 }
-*/
+
 
 @end
