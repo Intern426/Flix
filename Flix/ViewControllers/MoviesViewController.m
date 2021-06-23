fcd61f828c7539e0313c4a5a92a001fa6ee34e8d
Kalkidan Tamirat <kalkidant@fb.com>
2021-06-23 10:13:57 -0700

    Introduced Movie DB API using Network Request Sample Snippet

---
+++ Flix/ViewControllers/MoviesViewController.m
@@ -0,0 +1,59 @@
+//
+//  MoviesViewController.m
+//  Flix
+//
+//  Created by Kalkidan Tamirat on 6/23/21.
+//
+
+#import "MoviesViewController.h"
+
+@interface MoviesViewController ()
+@property (nonatomic, strong) NSArray *movies; //Automatically creates getter/setter method
+// Possible to override generated methods
+
+// Remember C doesn't have garbage collection so increment reference count of movies via strong paramater
+// Most everything we use will be nonatomic
+
+@end
+
+@implementation MoviesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+
+               NSLog(@"%@", dataDictionary);
+               
+               // TODO: Get the array of movies
+               self.movies = dataDictionary[@"results"];
+               for (NSDictionary *movie in self.movies) {
+                   NSLog(@"%@", movie[@"title"]);
+               }
+               
+               // TODO: Store the movies in a property to use elsewhere
+               // TODO: Reload your table view data
+           }
+       }];
+    [task resume];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
