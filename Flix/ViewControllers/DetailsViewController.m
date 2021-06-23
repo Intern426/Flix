f630cbc5be2167ed78666789f228b5899b0a169c
Kalkidan Tamirat <kalkidant@fb.com>
2021-06-23 14:41:53 -0700

    Title of movie and synopsis has been added to the movie detail screen

--- Flix/ViewControllers/DetailsViewController.m
+++ Flix/ViewControllers/DetailsViewController.m
@@ -41,6 +41,8 @@
     self.titleLabel.text = self.movie[@"title"];
     self.synopsisLabel.text = self.movie[@"overview"];
 
+    [self.titleLabel sizeToFit];
+    [self.synopsisLabel sizeToFit];
 }
 
 /*
